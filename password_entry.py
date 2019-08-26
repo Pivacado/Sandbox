33540393b8e6601c2dedce9934a732cdad237bc6
Adrian <adrian.pivotto@my.jcu.edu.au>
2019-08-26 22:45:50 +1000

    "Add password check program"

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,6 @@
 """Adrian"""
+min_length = 6
+password = input("Enter a password that is {} characters long".format(min_length))
+while len(password)< min_length:
+    password = input("Enter a password that is {} characters long".format(min_length))
+print('*' * len(password))
